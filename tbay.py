3a422093f4ba98791749745d2c716edec31fdf18
Zak652 <m2kzak@gmail.com>
2017-10-13 18:09:23 +0300

    Final tbay setup completed and ready for review

--- tbay.py
+++ tbay.py
@@ -1,15 +1,25 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from datetime import datetime
+
 
 engine = create_engine('postgresql://zak:thinkful@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime
+class User(Base):
+	__tablename__ = "users"
+
+	id = Column(Integer, primary_key = True)
+	user_name = Column(String, nullable = False)
+	password = Column(String, nullable = False)
+
+	items_owned = relationship("Item", backref = "users")
+	bids_placed = relationship("Bid", backref = "users")
+
 
 class Item(Base):
 	__tablename__ = "items"
@@ -19,18 +29,18 @@ class Item(Base):
 	description = Column (String)
 	start_time = Column(DateTime, default = datetime.utcnow)
 
-class User(Base):
-	__tablename__ = "users"
+	owner_id = Column(Integer, ForeignKey("users.id"), nullable = False)
 
-	id = Column(Integer, primary_key = True)
-	user_name = Column(String, nullable = False)
-	password = Column(String, nullable = False)
+	item_bids = relationship("Bid", backref = "items")
 
 class Bid(Base):
 	__tablename__ = "bids"
 
 	id = Column(Integer, primary_key = True)
 	price = Column(Integer, nullable = False)
+
+	bid_owner = Column(Integer, ForeignKey("users.id"), nullable = False)
+	item_bidded = Column(Integer, ForeignKey("items.id"), nullable = False)
 
 	
 Base.metadata.create_all(engine)
